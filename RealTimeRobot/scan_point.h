67595019f87f72ca108957ce05d6c7c4f4633f8a
zgl <285683264@qq.com>
2017-03-29 14:10:09 +0800

    扫描点云类

---
+++ RealTimeRobot/scan_point.h
@@ -0,0 +1,37 @@
+#include <Eigen/StdVector>
+#include <Eigen/Geometry>
+#include <pcl/PCLHeader.h>
+#include <pcl/exceptions.h>
+#include <pcl/point_traits.h>
+#include<vector>
+template <typename PointT>
+class PCL_EXPORTS ScanPoint
+{
+public:
+	pcl::PointCloud<pcl::PointXYZ> *scanpoint;
+	struct Surface {
+		double Area;
+		vector<float>Normal;
+		²ÎÊý£¿
+
+	};
+	void getArea(*ScanPoint, &Surface)
+
+		class KeyPoint {
+		public:
+			struct OccupiedGrid {
+				pcl::PointCloud<pcl::PointXYZ> *scanpoint;
+				int Number;
+				float Border[6];
+			};
+			void getOccupiedGrid(*ScanPoint, pcl::PointXYZ, &OccupiedGrid);
+			pcl::PointXYZ getKeypoint(*ScanPoint);
+			vector<vector<vector<double>>> vector3D(Surface S);
+	};
+	bool byHeight();
+	bool byArea();
+	bool byOccupied();
+
+};
+e
+
